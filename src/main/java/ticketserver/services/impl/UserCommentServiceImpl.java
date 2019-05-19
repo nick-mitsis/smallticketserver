9c83246e04dcc41f3b7d4588d46048813dfb2993
UngureanuSerg <ungureanuserg@gmail.com>
2019-05-19 23:52:07 +0300

    User comments implementation of basic methods

--- src/main/java/ticketserver/services/impl/UserCommentServiceImpl.java
+++ src/main/java/ticketserver/services/impl/UserCommentServiceImpl.java
@@ -1,8 +1,10 @@
 package ticketserver.services.impl;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import ticketserver.model.UserComments;
+import ticketserver.repositories.UserCommentsRepository;
 import ticketserver.services.UserCommentService;
 
 @Service
@@ -10,4 +12,15 @@ import ticketserver.services.UserCommentService;
 public class UserCommentServiceImpl extends BaseServiceImpl<UserComments>
     implements UserCommentService {
 
+  private final UserCommentsRepository<UserComments, String> userCommentsRepository;
+
+  @Override
+  public UserComments getEntity(String id) {
+    return getRepository().getOne(id);
+  }
+
+  @Override
+  public JpaRepository<UserComments, String> getRepository() {
+    return userCommentsRepository;
+  }
 }
